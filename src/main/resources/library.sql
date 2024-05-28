b7e71b1aa2db62e42bbc1dbbf3c4685ebffbc092
Mikel <mikelsanvi@gmail.com>
2024-05-28 12:35:58 +0200

    más tablas

--- src/main/resources/library.sql
+++ src/main/resources/library.sql
@@ -1,3 +1,5 @@
+DROP DATABASE IF EXISTS library;
+
 CREATE DATABASE library;
 
 USE library;
@@ -7,6 +9,27 @@ CREATE TABLE book(
 	title VARCHAR(50) NOT NULL,
 	author VARCHAR(50) NOT NULL,
 	year INT NOT NULL
+);
+
+CREATE TABLE customer(
+    nif VARCHAR(10) PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    surname VARCHAR(50) NOT NULL,
+    zip_code INT NOT NULL
+);
+
+CREATE TABLE book_lend(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    isbn VARCHAR(10) NOT NULL,
+    nif VARCHAR(10) NOT NULL,
+    lend_date DATE NOT NULL,
+    return_date DATE,
+    CONSTRAINT fk_book
+    FOREIGN KEY (isbn)
+    REFERENCES book(isbn),
+    CONSTRAINT fk_customer
+    FOREIGN KEY (nif)
+    REFERENCES customer(nif)
 );
 
 INSERT INTO book VALUES
